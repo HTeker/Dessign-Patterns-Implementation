92f7905b81311bd14b4d54b0085607b5dfb39c44
Halil Teker <h.teker.developer@gmail.com>
2017-06-25 20:32:40 +0200

    Addedd overridden prepare method to DHPizza

--- Opdrachten/src/SimpleFactory/DHPizza.java
+++ Opdrachten/src/SimpleFactory/DHPizza.java
@@ -4,4 +4,8 @@ package SimpleFactory;
  * Created by Halil Teker on 6/25/2017.
  */
 public abstract class DHPizza extends Pizza {
+    @Override
+    public void prepare() {
+        System.out.println("Adding ingredients to the pizza with a thin pizza base.");
+    }
 }
